1cf1294ef9c80994e083be45a7a59f9c77d924cc
thomazbastiaanssen <thomazbastiaanssen@gmail.com>
2020-08-14 17:49:26 +0200

    Update pairwise_aitchison_PERMANOVA.R
    Added access to R^2 and F-values from the vegan adonis function

--- R/pairwise_aitchison_PERMANOVA.R
+++ R/pairwise_aitchison_PERMANOVA.R
@@ -7,9 +7,14 @@ pairwise_aitchison_PERMANOVA <- function(clr.samples, groups, relevant.compariso
     comparison_title <- paste(relevant.comparisons[number,1], " VS ", relevant.comparisons[number,2])
     species.selected <- clr.samples[,groups == relevant.comparisons[number,1] |groups == relevant.comparisons[number,2]]
     dist.clr <- dist(t(species.selected))
-    ado <- adonis(dist.clr~relevant.groups, permutations = 999)
+    ado <- adonis(dist.clr~relevant.groups, permutations = 1000)
     out_df[1,number] = ado$aov.tab$`Pr(>F)`[1]
+    out_df[2, number] = ado$aov.tab$R2[1]
+    out_df[3, number] = ado$aov.tab$F.Model[1]
     colnames(out_df)[number] <- comparison_title
   }
+    row.names(out_df) <- c("p.value", 
+                         "R2", 
+                         "F.value")
   return(out_df)
 }
