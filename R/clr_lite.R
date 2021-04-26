818005338e90dd23907dff6d5eb13ce767b481bf
thomazbastiaanssen <thomazbastiaanssen@gmail.com>
2021-04-26 19:04:00 +0100

    Create clr_lite.R
    clr_lite uses either of the two simplest zero imputation methods mentioned by Sugnet Lubbe, Peter Filzmoser and Matthias Templ (https://doi.org/10.1016/j.chemolab.2021.104248) and then performs a clr transformation. 
    
    This method is much lighter than the ALDEx2 method and could be considered for very large datasets.

---
+++ R/clr_lite.R
@@ -0,0 +1,17 @@
+clr_lite = function(counts, samples_are = "cols", method = "unif"){
+  
+  if(! samples_are %in% c("rows", "cols")){stop("`samples_are` must be exactly `rows` or `cols`")}
+  
+  if(samples_are == "rows"){counts = t(counts)}
+  #impute zeroes
+    for(col in 1:ncol(counts)){
+      counts[,col] = impute_zeroes(vec = counts[,col], method = method)
+    }
+  if(samples_are == "rows"){counts = t(counts)}
+  #Apply CLR transformation  
+  if(samples_are == "cols"){counts = t(counts)}
+      clr_counts = data.frame(compositions::clr(counts))
+  if(samples_are == "cols"){clr_counts = t(clr_counts)}
+    
+    return(data.frame(clr_counts))
+    }
