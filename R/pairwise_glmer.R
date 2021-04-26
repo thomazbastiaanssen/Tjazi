7fb74c63469c73185f778bbafc75855ea46b5a56
thomazbastiaanssen <thomazbastiaanssen@gmail.com>
2021-04-26 23:38:54 +0100

    Update pairwise_glmer.R
    Updated glmer to match recent functionality upgrades of glm. May be a bit buggy have only done minor testing, but will do so in coming days.

--- R/pairwise_glmer.R
+++ R/pairwise_glmer.R
@@ -1,27 +1,53 @@
-pairwise_glmer <- function(clr,  
+pairwise_glmer <- function(clr, y = "microbe",
                            model = "~ . + (1|ID)", 
                            metadata, 
                            posthoc.method = "BH", 
-                           features.as.rownames = FALSE, CI = TRUE){
+                           features.as.rownames = FALSE, CI = TRUE, verbose = TRUE){
   #Generate output data.frame
   out_df = rbind()
   
-  #Run the models
-  for(feature in 1:nrow(clr)){
-    temp_df = metadata
-    temp_df$depend = unlist(clr[feature,])
-    fit = summary(lmerTest::lmer(formula = paste("depend", model, sep = " "),
-                      data    = temp_df))
-    
-    #Remove the "df" column to standardize results
-    fit_coef = coefficients(fit)[,!colnames(coefficients(fit)) == "df"]
-    
-    #Compute 95% Confidence interval and add to results
-    if(CI){fit_coef = cbind(fit_coef, car::Confint(lmer(formula = paste("depend",  model, sep = " "), data = temp_df))[,2:3])}
-    
-    #Collect the stats
-    out_df = rbind(out_df,  t(fit_coef)[1:length(fit_coef)])
-    
+  
+  if(y == "microbe"){
+    if(verbose){print(paste0("Running models with formula: microbe ", model))}
+    #Run the models
+    for(feature in 1:nrow(clr)){
+      temp_df = metadata
+      
+      
+      temp_df$microbe = unlist(clr[feature, ])
+      temp_df$depend  = unlist(clr[feature, ])
+      fit = summary(lmerTest::lmer(formula = paste("depend", model, sep = " "),
+                                   data    = temp_df))
+      #Remove the "df" column to standardize results
+      fit_coef = coefficients(fit)[, !colnames(coefficients(fit)) == "df"]
+      
+      #Compute 95% Confidence interval and add to results
+      if(CI){fit_coef = cbind(fit_coef, car::Confint(lmer(formula = paste("depend", model, sep = " "),
+                                                          data    = temp_df))[,2:3])}
+      #Collect the stats
+      out_df = rbind(out_df, t(fit_coef)[1:length(fit_coef)])
+    }
+  }
+  else if(y %in% colnames(metadata) ){
+    if(verbose){
+      print(paste("Running models with formula:",  y , model,  sep = " "))
+    }
+    #Run the models
+    for(feature in 1:nrow(clr)){
+      temp_df = metadata
+      temp_df$depend = unlist(clr[feature,])
+      fit = summary(lmerTest::lmer(formula = paste("depend", model, sep = " "),
+                                   data    = temp_df))
+      
+      #Remove the "df" column to standardize results
+      fit_coef = coefficients(fit)[,!colnames(coefficients(fit)) == "df"]
+      
+      #Compute 95% Confidence interval and add to results
+      if(CI){fit_coef = cbind(fit_coef, car::Confint(lmer(formula = paste("depend", model, sep = " "),
+                                                          data    = temp_df))[,2:3])}
+      #Collect the stats
+      out_df = rbind(out_df,  t(fit_coef)[1:length(fit_coef)])
+    }
   }
   
   #Reformat and name the stats part
@@ -35,12 +61,14 @@ pairwise_glmer <- function(clr,
   pvals <- colnames(out_df)[grepl("Pr\\(>", colnames(out_df))]
   df.bh <- out_df[, pvals]
   if(posthoc.method == "Storey"){
+    if(verbose){print("Adjusting for FDR using Storey's q-value procedure.")}
     colnames(df.bh) <- paste0(colnames(df.bh), ".Storeys.Q")
     for (j in 1:ncol(df.bh)) {
       df.bh[, j] <- qvalue::qvalue(p = df.bh[, j])$qvalues
     }
   } else {
     colnames(df.bh) <- paste(colnames(df.bh), posthoc.method, sep = ".")
+    if(verbose){print(paste0("Adjusting for FDR using ", posthoc.method, " procedure."))}
     for (j in 1:ncol(df.bh)) {
       df.bh[, j] <- p.adjust(df.bh[, j], method = posthoc.method)
     }
