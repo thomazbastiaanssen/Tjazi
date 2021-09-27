8d111c1fc4b5a1bdd72245ca7f11b42ef194c32b
thomazbastiaanssen <thomazbastiaanssen@gmail.com>
2021-09-27 15:20:18 +0100

    Update clr_lite.R

--- R/clr_lite.R
+++ R/clr_lite.R
@@ -1,16 +1,16 @@
 clr_lite = function(counts, samples_are = "cols", method = "unif"){
-  
+  temp_counts = counts
   if(! samples_are %in% c("rows", "cols")){stop("`samples_are` must be exactly `rows` or `cols`")}
   
-  if(samples_are == "rows"){counts = t(counts)}
+  if(samples_are == "rows"){temp_counts = t(temp_counts)}
   #impute zeroes
-    for(col in 1:ncol(counts)){
-      counts[,col] = impute_zeroes(vec = counts[,col], method = method)
+    for(col in 1:ncol(temp_counts)){
+      temp_counts[,col] = impute_zeroes(vec = temp_counts[,col], method = method)
     }
-  if(samples_are == "rows"){counts = t(counts)}
+  if(samples_are == "rows"){temp_counts = t(temp_counts)}
   #Apply CLR transformation  
-  if(samples_are == "cols"){counts = t(counts)}
-      clr_counts = data.frame(compositions::clr(counts))
+  if(samples_are == "cols"){temp_counts = t(temp_counts)}
+      clr_counts = data.frame(compositions::clr(temp_counts))
   if(samples_are == "cols"){clr_counts = t(clr_counts)}
     clr_counts = data.frame(clr_counts)
   
