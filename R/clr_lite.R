83d25394d20f89501d82aa3b4a0d5e99069f7753
thomazbastiaanssen <thomazbastiaanssen@gmail.com>
2021-11-19 16:58:30 +0000

    Update clr_lite.R
    Updated clr_lite to support sampling from a distribution as to stabilise measurements

--- R/clr_lite.R
+++ R/clr_lite.R
@@ -1,21 +1,26 @@
-clr_lite = function(counts, samples_are = "cols", method = "unif"){
+clr_lite = function(counts, samples_are = "cols", method = "logunif", replicates = 1000) 
+{
   temp_counts = counts
-  if(! samples_are %in% c("rows", "cols")){stop("`samples_are` must be exactly `rows` or `cols`")}
   
-  if(samples_are == "rows"){temp_counts = t(temp_counts)}
-  #impute zeroes
-    for(col in 1:ncol(temp_counts)){
-      temp_counts[,col] = impute_zeroes(vec = temp_counts[,col], method = method)
-    }
-  if(samples_are == "rows"){temp_counts = t(temp_counts)}
-  #Apply CLR transformation  
-  if(samples_are == "cols"){temp_counts = t(temp_counts)}
-      clr_counts = data.frame(compositions::clr(temp_counts))
-  if(samples_are == "cols"){clr_counts = t(clr_counts)}
-    clr_counts = data.frame(clr_counts)
+  if(! method %in% c("logunif", "unif", "const"))
+  {stop("`method` must be exactly `logunif`, `unif` or `const`")}
   
-    rownames(clr_counts) = rownames(counts)
-    colnames(clr_counts) = colnames(counts)
-
-    return(clr_counts)
-    }
+  if(samples_are == "rows"){
+    temp_counts = data.frame(t(temp_counts))
+  }
+  
+  temp_counts = apply(X          = temp_counts, 
+                      MARGIN     = 2, 
+                      FUN        = clr_imputed, 
+                      method     = method, 
+                      replicates = replicates)
+  
+  if(samples_are == "rows"){
+    temp_counts = data.frame(t(temp_counts))
+  }
+  
+  clr_counts = data.frame(temp_counts)
+  rownames(clr_counts) = rownames(counts)
+  colnames(clr_counts) = colnames(counts)
+  return(clr_counts)
+}
