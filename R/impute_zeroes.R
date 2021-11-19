4e40f24aa24f1554c77ed61833471410054dbf79
thomazbastiaanssen <thomazbastiaanssen@gmail.com>
2021-11-19 17:01:05 +0000

    Update impute_zeroes.R

--- R/impute_zeroes.R
+++ R/impute_zeroes.R
@@ -1,14 +1,16 @@
-impute_zeroes = function(vec, method = "unif"){
-  if(! method %in% c("unif", "const")){stop("`method` must be exactly `unif` or `const`")}
-
-  #Find detection limit
-  DL = min(vec[vec != 0])
-  
-  if(method == "unif"){
-     vec[vec == 0] = runif(n = sum(vec == 0), min =  0.1 * DL, max = DL)
-  }
-  else if(method == "const"){
-    vec[vec == 0] = 0.65 * DL
-}
-  return(vec)
-}
+impute_zeroes = function(vec, method = "logunif"){
+      if(! method %in% c("logunif", "unif", "const")){stop("`method` must be exactly `logunif`, `unif` or `const`")}
+      
+      #Find detection limit
+      DL = min(vec[vec != 0])
+      if(method == "logunif"){
+        vec[vec == 0] = DL/(10^(runif(n = sum(vec == 0), min =  0, max = 1)))
+      }
+      else if(method == "unif"){
+        vec[vec == 0] = runif(n = sum(vec == 0), min =  0, max = DL)
+      }
+      else if(method == "const"){
+        vec[vec == 0] = 0.65 * DL
+      }
+      return(vec)
+    } 
