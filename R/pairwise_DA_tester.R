8675684f86ed6ec9306ab76278611bf575dde161
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2022-05-09 16:29:58 +0100

    export some more functions

--- R/pairwise_DA_tester.R
+++ R/pairwise_DA_tester.R
@@ -1,9 +1,10 @@
-pairwise_DA_tester = function (clr, groups, comparisons, 
-                               verbose = TRUE, parametric = T, 
-                               ignore.posthoc = F, 
+#' @export
+pairwise_DA_tester = function (clr, groups, comparisons,
+                               verbose = TRUE, parametric = T,
+                               ignore.posthoc = F,
                                posthoc.method = "BH",
                                paired.test = FALSE){
-  
+
   #Define colnames for output
   pval_name = "p.value"
   if(!ignore.posthoc){
@@ -12,32 +13,32 @@ pairwise_DA_tester = function (clr, groups, comparisons,
       pval_name = "Q.value"
     }
   }
-  
+
   #Create output data.frame
   out_df = data.frame(microbe = rownames(clr))
-  
+
   for(comparison in 1:nrow(comparisons)){
-    
+
     #Name the comparison for output data.frame
-    comp_name <- paste(comparisons[comparison, 1], 
-                       comparisons[comparison, 2], 
+    comp_name <- paste(comparisons[comparison, 1],
+                       comparisons[comparison, 2],
                        sep = " vs ")
-    
+
     #Perform the appropriate t-test
     if(parametric){
       pvals = lapply(1:nrow(clr), function(i) t.test(
-        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,1]]))), 
-        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,2]]))), 
+        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,1]]))),
+        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,2]]))),
         paired = paired.test)[c("p.value")])
     } else {
       pvals = lapply(1:nrow(clr), function(i) wilcox.test(
-        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,1]]))), 
-        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,2]]))), 
+        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,1]]))),
+        as.numeric(as.character(unlist(clr[i,groups == comparisons[comparison,2]]))),
         paired = paired.test)[c("p.value")])
     }
     pvals = unlist(pvals)
-    
-    
+
+
     #Calculate appropriate effect size
     if(sum(c(groups == comparisons[comparison,1]),
            groups == comparisons[comparison,2]) > 40 ){
@@ -54,8 +55,8 @@ pairwise_DA_tester = function (clr, groups, comparisons,
         hedges.correction = TRUE)[c("estimate")])
     }
     evals = unlist(evals)
-    
-    
+
+
     #Perform FDR procedure
     if(!ignore.posthoc){
       if(posthoc.method == "Storey"){
@@ -64,17 +65,17 @@ pairwise_DA_tester = function (clr, groups, comparisons,
         pvals = p.adjust(p = pvals, method = posthoc.method)
       }
     }
-    
+
     #Gather results per comparison
-    comp_res = data.frame(a = unlist(pvals), 
-                          b = unlist(evals)) 
-    colnames(comp_res) = c(paste(comp_name, pval_name), 
+    comp_res = data.frame(a = unlist(pvals),
+                          b = unlist(evals))
+    colnames(comp_res) = c(paste(comp_name, pval_name),
                            comp_name)
-    
+
     #Add results to output
     out_df = cbind(out_df, comp_res)
   }
-  
+
   return(out_df)
-  
+
 }
--- R/pairwise_aitchison_PERMANOVA.R
+++ R/pairwise_aitchison_PERMANOVA.R
@@ -1,29 +1,28 @@
-pairwise_aitchison_PERMANOVA <- function (clr.samples, groups, relevant.comparisons, adjust.p = T, adj.method = "holm" ) 
+#' @export
+pairwise_aitchison_PERMANOVA <- function (clr.samples, groups, relevant.comparisons, adjust.p = T, adj.method = "holm" )
 {
-  library(vegan)
-  library(ALDEx2)
   out_df <- data.frame()
   for (number in 1:nrow(relevant.comparisons)) {
-    relevant.groups = groups[groups == relevant.comparisons[number, 
+    relevant.groups = groups[groups == relevant.comparisons[number,
                                                             1] | groups == relevant.comparisons[number, 2]]
-    comparison_title <- paste(relevant.comparisons[number, 
+    comparison_title <- paste(relevant.comparisons[number,
                                                    1], " VS ", relevant.comparisons[number, 2])
-    species.selected <- clr.samples[, groups == relevant.comparisons[number, 
+    species.selected <- clr.samples[, groups == relevant.comparisons[number,
                                                                      1] | groups == relevant.comparisons[number, 2]]
     dist.clr <- dist(t(species.selected))
-    ado <- adonis(dist.clr ~ relevant.groups, permutations = 1000)
+    ado <- vegan::adonis2(dist.clr ~ relevant.groups, permutations = 1000)
     out_df[1, number] = ado$aov.tab$`Pr(>F)`[1]
     out_df[2, number] = ado$aov.tab$R2[1]
     out_df[3, number] = ado$aov.tab$F.Model[1]
     colnames(out_df)[number] <- comparison_title
   }
   row.names(out_df) <- c("p.value", "R2", "F.value")
-  
+
   if(adjust.p){
-    out_df = rbind(p.adjust(out_df[1,], method = adj.method), 
+    out_df = rbind(p.adjust(out_df[1,], method = adj.method),
                    out_df)
     row.names(out_df) <- c("p.adjusted", "p.value", "R2", "F.value")
-    
+
   }
   return(out_df)
 }
