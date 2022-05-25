838b1c8baac8e6295a3ae77d54011dda5b0a63c5
thomaz <thomazbastiaansser@gmail.com>
2022-05-25 23:14:06 +0100

    refactored get_alpha_diversity wrapper

--- R/get_asymptotic_alpha.R
+++ R/get_asymptotic_alpha.R
@@ -1,32 +1,44 @@
 #' Functional wrapper to get the output of the wonderful iNext library in the format I use to pipe into ggplot2.
 #' @export
+get_asymptotic_alpha = function(species, verbose = TRUE){
 
-get_asymptotic_alpha = function(species, groups = c(), verbose = TRUE ){
-
-  #create output df
-  alpha_diversity <- data.frame(chao1              = rep(NA, ncol(species)),
-                                asymptotic_simps   = rep(NA, ncol(species)),
-                                asymptotic_shannon = rep(NA, ncol(species)))
-  row.names(alpha_diversity) = colnames(species)
-
-
-  for(samp in 1:ncol(species)){
-
-    raw_observation                            = species[,samp]
-    observations                               = raw_observation[raw_observation!= 0]
-    if(verbose){
-      print(paste("Processing sample ", samp, "of ", ncol(species)))
-    }
-    alpha_diversity[samp,"chao1"]              = iNEXT::ChaoSpecies(x = observations,datatype = "abundance")$Estimator
-    alpha_diversity[samp,"asymptotic_simps"]   = iNEXT::EstSimpson( x = observations,datatype = "abundance")$Estimator
-    alpha_diversity[samp,"asymptotic_shannon"] = iNEXT::ChaoEntropy(x = observations,datatype = "abundance")$Estimator
-    if(verbose){
-      print(paste("finished with sample ", samp, " out of ", ncol(species)))
-    }
+  if(any(colSums(species) < 1)){
+    if(verbose){print("reads seem to be relative abundance. This is not ideal. Applying transformation to address this.")}
+    species <- apply(species, 2, un_cpm)
   }
-  if(length(groups) == ncol(species)){
-    alpha_diversity <-(cbind(alpha_diversity, group = groups))
-  }
+  #create output df, use progress bar if available
+
+  alpha_diversity <- if(requireNamespace("pbapply", quietly = TRUE)) {
+    pbapply::pbapply(X = species,
+                     MARGIN =  2,
+                     FUN = div_applier) } else {
+               apply(X = species,
+                     MARGIN =  2,
+                     FUN = div_applier)
+                     }
+  alpha_diversity = data.frame(alpha_diversity)
+
+  colnames(alpha_diversity)  <- colnames(species)
+
+  alpha_diversity = cbind("Index" = c("Chao1", "Simpson Index", "Shannon Entropy"), alpha_diversity)
+
   return(alpha_diversity)
 }
+
+#' Functional wrapper to get the output of the wonderful iNext library in the format I use to pipe into ggplot2.
+#'
+div_applier <- function(x){
+  out_div <- vector(mode = "numeric", length = 3)
+
+  out_div[1] <- iNEXT::ChaoRichness(x)$Estimator
+  out_div[2] <- iNEXT::ChaoSimpson( x)$Estimator
+  out_div[3] <- iNEXT::ChaoEntropy( x)$Estimator
+
+  return(out_div)
+}
+
+
+#' Undo CPM transformation
+#'
+un_cpm <- function(x) 1E6 * x/sum(x)
 
