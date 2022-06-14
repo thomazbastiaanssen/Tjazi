1b186f21aa08a126a45124848af7839d75c1df95
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2022-06-14 14:20:43 +0100

    Added some data for demonstration purposes.

---
+++ R/guidebook_data.R
@@ -0,0 +1,38 @@
+#' The curated and aggregated genus-level count table from the schizophrenia study for demonstration
+#'
+#' @description A genus-level count table for instructional purposes. Accessed using the curatedMetagenomicData R package
+#'
+#' @format A data.frame object with 162 rows, genera, and  171 columns, samples.
+#' @source \url{https://doi.org/10.1038/s41467-020-15457-9}
+#'
+"counts"
+
+
+#' The curated Gut-Brain modules count table from the schizophrenia study for demonstration
+#'
+#' @description A Gut-Brain modules count table for instructional purposes. Accessed using the curatedMetagenomicData R package
+#'
+#' @format A data.frame object with 43 rows, Gut-Brain modules, and  171 columns, samples.
+#' @source \url{https://doi.org/10.1038/s41467-020-15457-9}
+#'
+"GBMs"
+
+
+#' The curated Gut-Metabolic modules count table from the schizophrenia study for demonstration
+#'
+#' @description A Gut-Metabolic modules count table for instructional purposes. Accessed using the curatedMetagenomicData R package
+#'
+#' @format A data.frame object with 98 rows, Gut-Metabolic, and  171 columns, samples.
+#' @source \url{https://doi.org/10.1038/s41467-020-15457-9}
+#'
+"GMMs"
+
+
+#' A snippet of the metadata from the schizophrenia study
+#'
+#' @description Contains the per-sample information for the samples from the schizophrenia study. Accessed using the curatedMetagenomicData R package
+#'
+#' @format A data.frame object with 171 rows, samples, and 10 columns, denoting sample ID, schizophrenia status, sex, BMI, smoking status and some additional information from the original metadata.
+#' @source \url{https://doi.org/10.1038/s41467-020-15457-9}
+#'
+"metadata"
